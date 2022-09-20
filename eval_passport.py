7af9adf0612d7577341b21b17658d9fa4da8eebc
awhamil <andrew.hamilton@gmail.com>
2022-09-20 09:23:35 -0400

    code cleanup

--- eval_passport.py
+++ eval_passport.py
@@ -1,9 +1,8 @@
+print('Loading...')
+
 # basic python and ML Libraries
-import os
 import sys
-import random
 import numpy as np
-import pandas as pd
 
 # for ignoring warnings
 import warnings
@@ -15,15 +14,10 @@ import cv2
 # torchvision libraries
 import torch
 import torchvision
-from torchvision import transforms as torchtrans  
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 # for image augmentations
 from albumentations.pytorch.transforms import ToTensorV2
-
-# matplotlib for visualization
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 image_path = sys.argv[1]
 
@@ -37,31 +31,7 @@ def apply_nms(orig_prediction, iou_thresh=0.3):
   final_prediction['labels'] = final_prediction['labels'][keep]
   return final_prediction
 
-# function to convert a torchtensor back to PIL image
-def torch_to_pil(img):
-  return torchtrans.ToPILImage()(img).convert('RGB')
-
-# Function to visualize bounding boxes in the image
-def plot_img_bbox(img, target):
-  # plot the image and bboxes
-  # Bounding boxes are defined as follows: x-min y-min width height
-  fig, a = plt.subplots(1,1)
-  fig.set_size_inches(5,5)
-  a.imshow(img)
-  for box in (target['boxes']):
-    x, y, width, height  = box[0], box[1], box[2]-box[0], box[3]-box[1]
-    rect = patches.Rectangle(
-      (x, y),
-      width, height,
-      linewidth = 2,
-      edgecolor = 'r',
-      facecolor = 'none'
-    )
-    # Draw the bounding box on top of the image
-    a.add_patch(rect)
-  plt.show()
-
-# read image and convert to correct size and color    
+# read image and convert to correct size and color  
 img = cv2.imread(image_path)
 h, w, c = img.shape
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
@@ -70,16 +40,22 @@ img = ToTensorV2(p=1.0)(image=img)['image']
 
 # load a model pre-trained pre-trained on COCO
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-# get number of input features for the classifier
-in_features = model.roi_heads.box_predictor.cls_score.in_features
+
 # replace the pre-trained head with a new one
+in_features = model.roi_heads.box_predictor.cls_score.in_features
 num_classes = 2
 model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
 
+# load parameters that were trained on locating mrz areas
 model.load_state_dict(torch.load('model.dict', map_location=torch.device('cpu')))
 model.eval()
+
+# get predictions for current image
+print('Predicting...')
 with torch.no_grad():
   prediction = model([img])[0]
+
+# remove extra bounding boxes
 nms_prediction = apply_nms(prediction, iou_thresh=0.01)
+
 print(nms_prediction)
-#plot_img_bbox(torch_to_pil(img), nms_prediction)
